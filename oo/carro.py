beb933d0eaf137bb5a090ebaec013cbcb30bffd3
Hellen Ingrid <helleningrid2018@gmail.com>
2022-08-17 15:06:15 -0300

    Imprementada a direçao

--- oo/carro.py
+++ oo/carro.py
@@ -103,7 +103,30 @@ class Motor:
         self.velocidade = max(0, self.velocidade)
 
 
+NORTE = 'Norte'
+SUL = 'Sul'
+LESTE = 'Leste'
+OESTE = 'Oeste'
 
+
+class Direcao:
+
+    rotacao_a_direita_dct = {
+        NORTE: LESTE, LESTE: SUL, SUL: OESTE, OESTE: NORTE
+    }
+
+    rotacao_a_esquerda_dct = {
+        NORTE: OESTE, OESTE: SUL, SUL: LESTE, LESTE: NORTE
+    }
+
+    def __init__(self):
+        self.valor = NORTE
+
+    def girar_a_direita(self):
+        self.valor = self.rotacao_a_direita_dct[self.valor]
+
+    def girar_a_esquerda(self):
+        self.valor = self.rotacao_a_esquerda_dct[self.valor]
 
 
 
